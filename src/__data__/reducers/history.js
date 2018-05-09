e357c007aacea4a6735f51e4d9537692479a95aa
aleksandr <alx.panteleev@gmail.com>
2018-05-09 11:03:23 +0200

    repair history

--- src/__data__/reducers/history.js
+++ src/__data__/reducers/history.js
@@ -1,4 +1,4 @@
-import { SET_QUESTION_ID, GO_BACK } from '../action-types';
+import { SET_QUESTION_ID, GO_BACK, SUBMIT_STEP } from '../action-types';
 
 const initialState = [];
 
@@ -6,6 +6,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case SET_QUESTION_ID:
       return [...state, action.id];
+    case SUBMIT_STEP:
+      return [...state, action.nextId]
     case GO_BACK:
       return state.slice(0, state.length - 1);
     default:
